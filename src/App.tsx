eb9955b05b7755ad9a60eff8e717fdf25129a6eb
Hxgh <lhygodlike@gmail.com>
2019-11-22 10:45:21 +0800

    第一次建立模板

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Button from 'antd/es/button';
+import './App.css';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Button type="primary">Button</Button>
+    </div>
+  );
+};
+
+export default App;
