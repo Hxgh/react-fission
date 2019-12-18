224ebc38e94126b05019d0cc0e6e3410da70e5f2
hxgh <lhygodlike@gmail.com>
2019-12-18 23:51:53 +0800

    修改index 入口

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./App.less";
+import RouterView from "./router";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <RouterView />
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.less';
-import App from './router';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 serviceWorker.unregister();
